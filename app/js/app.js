e236a21d78557297eb2a74403d99912c161a9c43
jirishus <jirishus@yahoo.com>
2014-12-08 18:52:57 -0800

    mids payment types ng-sanitize

--- app/js/app.js
+++ app/js/app.js
@@ -1,4 +1,4 @@
-var app = angular.module("myApp", ['ui.router','LocalStorageModule','angular-loading-bar','ui.bootstrap','smart-table']);
+var app = angular.module("myApp", ['ui.router','LocalStorageModule','angular-loading-bar','ui.bootstrap','smart-table','ngSanitize',]);
 
 
 app.constant('ngAuthSettings', {
