191abbd89a6f3497aa383ed866d61e1ce388e643
jirishus <jirishus@yahoo.com>
2014-12-08 22:00:00 -0800

    angular-wizard/lodash mid create

--- app/js/app.js
+++ app/js/app.js
@@ -1,4 +1,4 @@
-var app = angular.module("myApp", ['ui.router','LocalStorageModule','angular-loading-bar','ui.bootstrap','smart-table','ngSanitize',]);
+var app = angular.module("myApp", ['ui.router','LocalStorageModule','angular-loading-bar','ui.bootstrap','smart-table','ngSanitize','mgo-angular-wizard']);
 
 
 app.constant('ngAuthSettings', {
